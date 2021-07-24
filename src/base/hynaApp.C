30c9131a9a8557245121015b874735b2212a0767
Pushkar Anirudha Pandit <pushkarpandit95@gmail.com>
2021-07-24 11:03:38 +0530

    Initial files

---
+++ src/base/hynaApp.C
@@ -0,0 +1,53 @@
+#include "hynaApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+hynaApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+
+  // Do not use legacy material output, i.e., output properties on INITIAL as well as TIMESTEP_END
+  params.set<bool>("use_legacy_material_output") = false;
+
+  return params;
+}
+
+hynaApp::hynaApp(InputParameters parameters) : MooseApp(parameters)
+{
+  hynaApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+hynaApp::~hynaApp() {}
+
+void
+hynaApp::registerAll(Factory & f, ActionFactory & af, Syntax & syntax)
+{
+  ModulesApp::registerAll(f, af, syntax);
+  Registry::registerObjectsTo(f, {"hynaApp"});
+  Registry::registerActionsTo(af, {"hynaApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+hynaApp::registerApps()
+{
+  registerApp(hynaApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+hynaApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  hynaApp::registerAll(f, af, s);
+}
+extern "C" void
+hynaApp__registerApps()
+{
+  hynaApp::registerApps();
+}

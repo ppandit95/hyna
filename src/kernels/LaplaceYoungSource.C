00084ba2c3d2d003f6ae868bfc85892ab18a27ad
Pushkar Anirudha Pandit <pushkarpandit95@gmail.com>
2021-07-24 14:08:54 +0530

    Added necessary files for LaplaceYoungEquation

---
+++ src/kernels/LaplaceYoungSource.C
@@ -0,0 +1,23 @@
+#include "LaplaceYoungSource.h"
+
+registerMooseObject("HynaMooseApp", LaplaceYoungSource);
+
+InputParameters
+LaplaceYoungSource::validParams()
+{
+  auto params = ADKernelValue::validParams();
+  params.addParam<Real>("kappa",1,"Coefficient to the laplace Young Equation source term");
+  params.addClassDescription("Source contribution in Laplace Young Equation");
+  return params;
+}
+
+LaplaceYoungSource::LaplaceYoungSource(const InputParameters & parameters) : ADKernelValue(parameters),
+_kappa(getParam<Real>("kappa"))
+{}
+
+ADReal
+LaplaceYoungSource::precomputeQpResidual()
+{
+  
+  return _kappa * _u[_qp];
+}
